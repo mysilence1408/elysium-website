891267a8d8bd16b7093305532f9fd6cf36aed3c1
mysilence1408 <mysilence1408@gmail.com>
2025-10-31 21:03:55 +0200

    lenis completed

--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -2,17 +2,28 @@
 import { Footer } from "@/components/Global/Footer";
 import Navbar from "@/components/Global/Navbar";
 import { SessionProvider } from "next-auth/react";
-import React from "react";
+import { ReactLenis, useLenis } from "./lenis";
+import { usePathname, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const lenis = useLenis();
+
+  useEffect(() => {
+    lenis?.scrollTo(0, { immediate: true });
+  }, [pathname, searchParams, lenis]);
   return (
-    <SessionProvider>
-      <Toaster position="top-center" reverseOrder={false} />
-      <Navbar />
-      <main className=" pt-14 md:pt-16">{children}</main>
-      <Footer />
-    </SessionProvider>
+    <ReactLenis root options={{ lerp: 0.05 }}>
+      <SessionProvider>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Navbar />
+        <main className=" pt-14 md:pt-16">{children}</main>
+        <Footer />
+      </SessionProvider>
+    </ReactLenis>
   );
 };
 
