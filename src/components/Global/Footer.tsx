9a38ac24e576bc54a6f2809bd7bddfe145424edd
mysilence1408 <mysilence1408@gmail.com>
2025-11-09 20:25:40 +0200

    footer social icons

--- src/components/Global/Footer.tsx
+++ src/components/Global/Footer.tsx
@@ -1,6 +1,12 @@
-import { ReactNode } from "react";
+import React from "react";
 import { Link } from "next-view-transitions";
 import Image from "next/image";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaXTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
 
 export const Footer = () => {
   return (
@@ -9,94 +15,27 @@ export const Footer = () => {
         Footer
       </h2>
       <div className="container mx-auto px-6">
-        <div className="grid gap-10 md:grid-cols-3">
-          <NavGroup title="Shop">
-            <NavLink href="/fragrance/terra">Terra</NavLink>
-            <NavLink href="/fragrance/igni">Igni</NavLink>
-            <NavLink href="/fragrance/aqua">Aqua</NavLink>
-          </NavGroup>
-
-          <NavGroup title="About">
-            <NavLink href="#">Science</NavLink>
-            <NavLink href="#">Our Story</NavLink>
-            <NavLink href="#">Côte Royale</NavLink>
-          </NavGroup>
-
-          <NavGroup title="Social">
-            <NavLink href="#">Instagram</NavLink>
-            <NavLink href="#">X (Twitter)</NavLink>
-            <NavLink href="#">Facebook</NavLink>
-          </NavGroup>
-        </div>
-
         {/* Bottom footer */}
-        <div className="mt-16 flex flex-col items-center justify-between gap-6 border-t border-neutral-800 pt-8 md:flex-row">
+        <div className="flex flex-col items-center justify-between gap-6 border-t border-neutral-800 pt-8 md:flex-row">
           <p className="text-center text-sm text-gray-400">
-            © {new Date().getFullYear()} Côte Royale Inc. All rights reserved
+            © {new Date().getFullYear()} Elysium. All rights reserved
           </p>
-          <Link
-            href="/"
-            aria-label="Côte Royale Home"
-            className="order-first md:order-none"
-          >
+          <Link href="/">
             <Image
               src="/images/logo.png"
-              alt="CÔTE ROYALE"
+              alt="Elysium"
               width={150}
               height={25}
             />
           </Link>
-          <ul
-            aria-label="Legal"
-            className="flex flex-wrap justify-center gap-6 text-sm text-gray-400"
-          >
-            <li>
-              <Link href="#" className="hover:text-white">
-                Terms &amp; conditions
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className="hover:text-white">
-                Privacy Policy
-              </Link>
-            </li>
-          </ul>
+          <div className=" flex items-center gap-2 cursor-pointer text-gray-400">
+            <FaFacebook />
+            <FaInstagram />
+            <FaYoutube />
+            <FaXTwitter />
+          </div>
         </div>
       </div>
     </footer>
-  );
-};
-
-type NavGroupProps = {
-  title: string;
-  children?: ReactNode;
-};
-
-const NavGroup = ({ title, children }: NavGroupProps) => (
-  <nav aria-labelledby={`${title.toLowerCase()}-heading`}>
-    <h3
-      id={`${title.toLowerCase()}-heading`}
-      className="mb-6 text-xl font-medium"
-    >
-      {title}
-    </h3>
-    <ul className="space-y-4" role="list">
-      {children}
-    </ul>
-  </nav>
-);
-
-type NavLinkProps = {
-  href: string;
-  children: ReactNode;
-};
-
-const NavLink = ({ href, children }: NavLinkProps) => {
-  return (
-    <li>
-      <Link href={href} className="hover:text-gray-300">
-        {children}
-      </Link>
-    </li>
   );
 };
